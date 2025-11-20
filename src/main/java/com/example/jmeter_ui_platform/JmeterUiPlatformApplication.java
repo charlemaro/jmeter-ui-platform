04d1fe361a764e5ac473febf210694675ffaf756
Charle Maro <charlemaro7@gmail.com>
2025-11-20 10:52:54 +0530

    Added backend Feautures

---
+++ src/main/java/com/example/jmeter_ui_platform/JmeterUiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.jmeter_ui_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterUiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeterUiPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jmeter_ui_platform/JmeterUiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jmeter_ui_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmeterUiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
